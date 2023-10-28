3b2542bf2b2a2a9d88240b031adbefa74e177285
Karthik <124011389+karthiksaki@users.noreply.github.com>
2023-10-28 10:53:44 -0500

    Update ec2_using_resource.py

--- ec2_using_resource.py
+++ ec2_using_resource.py
@@ -1,9 +1,8 @@
-import boto3r
+import boto3
 from pprint import pprint
-
 aws_console = boto3.Session(profile_name="default")
 ec2_resource = aws_console.resource("ec2")
 ec2_client = aws_console.client('ec2')
 for i in ec2_resource.instances.all():
     print(i.id,i.state['Name'])
-    print(f"Availability Zone: {i.placement['AvailabilityZone']}")  # Access the 'AvailabilityZone' attribute of 'placement' dictionary
+    print(f"Availability Zone: {i.placement['AvailabilityZone']}")  # Access the 'AvailabilityZone' attribute of the 'placement' dictionary
